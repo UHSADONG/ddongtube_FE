dbe533c348535632b2cc4b148aaeccd2b20f6a8f
jpark0506 <peterp1@inha.edu>
2025-03-26 01:05:57 +0900

    [USD-12] chore : 프로젝트 초기 구성

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-full flex flex-col items-center justify-center'>
+      <h1 className='text-5xl font-bold'>Deeeply</h1>
+    </div>
+  )
+}
+
+export default App
