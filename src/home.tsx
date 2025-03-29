8480ad7bc8da3d416ec195817e621d40b8dac5fa
jpark0506 <peterp1@inha.edu>
2025-03-30 00:10:30 +0900

    [USD-17] feat : Route 연결

---
+++ src/home.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+type Props = {}
+
+const home = (props: Props) => {
+    return (
+        <div className='w-full h-full flex flex-col items-center justify-center'>
+            <h1 className='text-5xl font-bold'>Deeeply</h1>
+            <button
+                type="button"
+                className='mt-4 bg-blue-500 text-white px-4 py-2 rounded'
+                onClick={() => {
+                    throw new Error("Sentry Test Error");
+                }}
+            >
+                Break the world
+            </button>
+        </div>
+    )
+}
+
+export default home
