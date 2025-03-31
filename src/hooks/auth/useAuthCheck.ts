05e0351aa8cf1e1e2c7b52cff681d182254bafd3
jpark0506 <peterp1@inha.edu>
2025-04-01 00:12:26 +0900

    [USD-52] feat : Auth Check 훅 생성

---
+++ src/hooks/auth/useAuthCheck.ts
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react'
+import { getSessionStorage, removeSessionStorage } from '../../utils/sessionStorage';
+import { useNavigate } from 'react-router';
+
+export const useAuthCheck = () => {
+
+    const navigate = useNavigate();
+    const storage = getSessionStorage();
+    useEffect(() => {
+        if (!storage || !storage?.playlistCode) {
+            removeSessionStorage();
+            navigate("/start");
+        }
+    }, []);
+    return {
+        authCheck: storage ? true : false,
+        playlistCode : storage?.playlistCode,
+    }
+}   
