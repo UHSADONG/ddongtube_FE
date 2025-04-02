60d01231a3aa6556c3552d0e550a87a262570d78
Junhyeok Park(박준혁) <peterp1@inha.edu>
2025-04-02 18:36:17 +0900

    [USD-63] feat : PWA 설정 완료 (#16)
    * [USD-63] feat : PWA 설정 완료
    
    * [USD-63] feat : AI 제안 수정사항 반영

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,43 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), sentryVitePlugin({
+  plugins: [
+    react(),
+    tailwindcss(),
+    sentryVitePlugin({
     org: "jpark",
-    project: "deeeply-fe"
-  }),svgr()],
+    project: "deeeply-fe"}),
+    svgr(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png'],
+      manifest: {
+        name: 'Deeeply',
+        short_name: '디플리',
+        description: '안 들어오면..나..24시간 뒤에..터진다?ㅋ',
+        icons: [
+          {
+            src: './favicon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: './favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+        start_url: '/start',
+        display: 'standalone',
+        background_color: '#1F1F20',
+        theme_color: '#D93F0F'
+      }
+     })
+  ],
 
   build: {
     sourcemap: true
