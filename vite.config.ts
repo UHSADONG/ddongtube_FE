dbc0b55a8f5828ae2b4da3f5e46f1d90958b7586
jpark0506 <peterp1@inha.edu>
2025-04-02 18:27:34 +0900

    [USD-63] feat : PWA 설정 완료

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), sentryVitePlugin({
+  plugins: [
+    react(),
+    tailwindcss(),
+    sentryVitePlugin({
     org: "jpark",
-    project: "deeeply-fe"
-  }),svgr()],
+    project: "deeeply-fe"}),
+    svgr(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png'],
+     })
+  ],
 
   build: {
     sourcemap: true
