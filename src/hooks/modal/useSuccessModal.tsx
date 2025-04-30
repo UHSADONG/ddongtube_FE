9c065521cb80b187a0840b202fd2d94ef33faf67
jpark0506 <peterp1@inha.edu>
2025-05-01 03:20:19 +0900

    [USD-83] feat : 버튼 hover, action 설정

--- src/hooks/modal/useSuccessModal.tsx
+++ src/hooks/modal/useSuccessModal.tsx
@@ -22,11 +22,11 @@ export default function useModal(
 
             return (
                 <div className="fixed inset-0 flex items-center justify-center bg-[rgba(31,31,32,0.6)]">
-                    <div className="w-full max-w-[400px] mx-6 flex flex-col bg-fill rounded-400 overflow-hidden items-center justify-center px-4 pb-7 pt-8  rounded-[20px]">
+                    <div className="w-full max-w-[400px] mx-6 flex flex-col bg-fill rounded-400 overflow-hidden items-center justify-center px-4 pb-7 pt-8 rounded-[20px]">
                         <div className="py-[25px]">{children}</div>
                         <button
                             onClick={close}
-                            className="w-fit h-fit text-text-large-bold font-bold text-white py-3 px-6 rounded-xl bg-main hover:bg-main-focus"
+                            className="w-fit h-fit text-text-large-bold font-bold text-white py-3 px-6 rounded-xl bg-main hover:bg-main-focus active:bg-main-focus transition duration-200 ease-in-out"
                         >
                             확인
                         </button>
